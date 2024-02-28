e686f0a79e2d24be0daca23732950552c766ebcb
Milenkov <131610937+KristiyanMilenkov@users.noreply.github.com>
2024-02-28 11:49:50 +0100

    Code first

---
+++ GringottsDatabase/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("main");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
